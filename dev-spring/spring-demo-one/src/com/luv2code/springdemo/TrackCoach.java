4e8d788b2111037e3c4b9b3a97dc0eaa019bcc0e
katon user <gjpavip@gmail.com>
2019-04-21 03:28:09 -0300

    Dependency injection

--- dev-spring/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ dev-spring/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,10 +1,25 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
-
+	
+	// Define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// Define a constructor for the DI
+	public TrackCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		// Use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
